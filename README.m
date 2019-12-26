214394a88a3483d4af6f29de9db08b0efcbc8430
Highlander Paiva <highlander.vilela@usemobile.xyz>
2019-12-26 14:05:47 -0300

    initial commit

---
+++ README.m
@@ -0,0 +1,45 @@
+![GitHub](https://img.shields.io/github/license/usemobile/vscode-reactjs-snippets.svg)
+
+<!-- ABOUT THE PROJECT -->
+
+## Sobre o Projeto
+
+Este projeto visa a disponibilização de um conjunto de Snippets ou atalhos para criação de componentes e arquivos de configuração em aplicações ReactJS.
+
+## Começando
+
+### Instalação
+
+Para instalar uma extensão você pode executar o **Command Pallete** através do comando `Ctrl + Shift + P` ou `Cmd + Shift + P` , digitar `Install Extensions` e por fim dar um `Enter`, feito isso pesquise por Rocketseat e você vai encontrar a extensão **Rocketseat ReactJS**.
+
+### Linguagens Suportadas
+
+- JavaScript (.js)
+- TypeScript (.ts)
+- JavaScript React (.jsx)
+- TypeScript React (.tsx)
+
+### Snippets
+
+Abaixo segue a lista com todos os Snippets disponíveis e os gatilhos para cada um. O **⇥** significa a tecla `TAB`.
+
+|                    Gatilho | Conteúdo                                                                      |
+| -------------------------: | ----------------------------------------------------------------------------- |
+|                     `rc →` | Cria um Componente **Stateless** enxuto                                       |
+|                    `rsc →` | Cria um Componente **Stateless**                                              |
+|              `rc-styled →` | Cria um arquivo de Estilização com **Styled Components**                      |
+|             `rc-reducer →` | Cria o arquivo de reducer                                                     |
+|                `rc-saga →` | Cria o arquivo de Sagas                                                       |
+
+
+<!-- LICENSE -->
+
+## Licença
+
+Distribuído sob a licença MIT. Veja `LICENSE` para mais informações.
+
+<!-- CONTACT -->
+
+## Contato
+
+Usemobile - [Github](https://github.com/usemobile)
